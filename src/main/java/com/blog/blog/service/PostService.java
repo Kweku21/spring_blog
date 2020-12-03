c97dd100d60e8ca8b151f0f76a682c991831a3b1
Kweku Debrah <edebrah@npontu,com>
2020-12-03 20:56:25 +0000

    first

---
+++ src/main/java/com/blog/blog/service/PostService.java
@@ -0,0 +1,12 @@
+package com.blog.blog.service;
+
+import com.blog.blog.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+}
